8893101f83200521090a6f5c610a5bdc5d22d2d9
林池 <2226564833@qq.com>
2020-12-25 11:29:25 +0800

    init

---
+++ src/main/java/com/picturecontrolapi/picturecontrolapi/PictureControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.picturecontrolapi.picturecontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureControlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureControlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picturecontrolapi/picturecontrolapi/PictureControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picturecontrolapi.picturecontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureControlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
